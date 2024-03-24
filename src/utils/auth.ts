54971096a0030e5207953945d1a9ad066ddb798d
thaitran003 <thai.txt.03@gmail.com>
2024-03-24 16:48:21 +0700

    ud

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -24,8 +24,8 @@ export const authOptions: NextAuthOptions = {
     },
     providers: [
         Facebook({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!,
+            clientId: process.env.FACEBOOK_CLIENT_ID!,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
         })
     ],
     callbacks: {
