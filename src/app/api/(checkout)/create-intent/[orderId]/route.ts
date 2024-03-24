af7781adac54b5a59cfaf6723102512c87a9711b
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2024-03-24 22:03:52 +0700

    fix(create-intent): fix bug stripe

--- src/app/api/(checkout)/create-intent/[orderId]/route.ts
+++ src/app/api/(checkout)/create-intent/[orderId]/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(
   request: NextRequest,
